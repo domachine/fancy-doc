ff3b225eb9260e6225036df95c81bcf7a851c595
Dominik Burgdörfer <dominik.burgdoerfer@mikanda.de>
2013-07-01 10:38:33 +0200

    Added submodule grouping.

--- index.js
+++ index.js
@@ -3,10 +3,33 @@
  * Module dependencies.
  */
 
-var express = require('express');
+var fs = require('fs')
+  , express = require('express')
+  , Builder = require('component-builder')
+
+  // symbol imports
+
+  , writeFile = fs.writeFileSync;
 
 // create application
 
 var app = module.exports = express();
+app.use(express.favicon());
 app.use(require('project-view'));
+app.use(build);
 app.use(express.static('public'));
+
+/**
+ * Build the components.
+ */
+
+function build(req, res, next) {
+  var builder = new Builder('.');
+  builder.copyAssetsTo('public');
+  builder.build(function(err, res){
+    if (err) return next(err);
+    writeFile('public/app.js', res.require + res.js);
+    writeFile('public/app.css', res.css);
+    next();
+  });
+}
